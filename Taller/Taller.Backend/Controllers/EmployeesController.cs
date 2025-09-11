ef2e65e42db2d569ea3fdf2be884d392b4957987
Ilder <Ilder@LAPTOP-3ULK8UF3>
2025-09-11 17:38:44 -0500

    Initial Migration & data context

---
+++ Taller/Taller.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Taller.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class EmployeesController : ControllerBase
+{
+    public EmployeesController()
+    {
+        
+    }
+}
---
+++ Taller/Taller.Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taller.Shared.Entities;
+
+namespace Taller.Backend.Data;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Taller.Shared.Entities.Employee> Employees { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Employee>().HasIndex(x => new { x.FirstName, x.LastName }).IsUnique();
+        {
+        }
+    }
+}
+
