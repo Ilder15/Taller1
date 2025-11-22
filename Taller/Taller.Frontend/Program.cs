705b5893a0c6ae096c7f9f7123671c87e9e61b4b
Ilder <Ilder@LAPTOP-3ULK8UF3>
2025-11-22 15:37:28 -0500

    Add Register

--- Taller/Taller.Frontend/Program.cs
+++ Taller/Taller.Frontend/Program.cs
@@ -3,6 +3,7 @@ using MudBlazor.Services;
 using Taller.Frontend.AuthenticationProviders;
 using Taller.Frontend.Components;
 using Taller.Frontend.Repositories;
+using Taller.Frontend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +15,13 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddSingleton(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7213") });
 builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+
 
 var app = builder.Build();
 
